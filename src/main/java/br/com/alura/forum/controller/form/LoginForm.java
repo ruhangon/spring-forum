50c4d24050cbbe1321d355be2034440dc14626f2
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2021-02-24 17:56:39 -0300

    Implementa geração de token para fazer autenticação

---
+++ src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -0,0 +1,18 @@
+package br.com.alura.forum.controller.form;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginForm {
+	private String email;
+	private String senha;
+
+	public UsernamePasswordAuthenticationToken converter() {
+		return new UsernamePasswordAuthenticationToken(email, senha);
+	}
+
+}
